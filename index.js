def017ec16f9becfcb6b1f56153d9d55d98e7882
somasekhar14002 <somasekhar14002@gmail.com>
2025-12-01 23:51:52 +0530

    okay

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hi somasekhar reddy\n');
+  res.end('Hi somasekhar reddy bontha\n');
 });
 
 server.listen(port, () => {
